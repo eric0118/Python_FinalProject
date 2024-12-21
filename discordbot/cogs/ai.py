00569c49d6715ac06431ed17f35a1bd485104b51
howardmeow <98642453+howardmeow@users.noreply.github.com>
2024-12-21 13:33:20 +0800

    Update ai.py
    將AI的安全性能關掉

--- discordbot/cogs/ai.py
+++ discordbot/cogs/ai.py
@@ -2,10 +2,17 @@ import discord
 from discord.ext import commands
 import textwrap
 import google.generativeai as genai
+from google.generativeai.types import HarmCategory, HarmBlockThreshold
 from IPython.display import display
 from IPython.display import Markdown
+from dotenv import load_dotenv
+import os
 
-genai.configure(api_key="api_key")
+load_dotenv()
+
+AI_API_KEY = os.getenv("AI_API_KEY")
+
+genai.configure(api_key=AI_API_KEY)
 model = genai.GenerativeModel('gemini-1.5-flash')
 
 class AI(commands.Cog):
@@ -15,7 +22,15 @@ class AI(commands.Cog):
     # 前綴指令
     @commands.command(help = "採用Gemini-1.5-flash模型，只要在指令後面加上你的問題，就可以問它任何問題", brief = "+ 你想問的問題，採用Gemini-1.5-flash模型，幫你解決問題")
     async def ai(self, ctx: commands.Context, question):
-        response = model.generate_content(question)
+        response = model.generate_content(
+            question,
+            safety_settings={
+                HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+                HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+                HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+                HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+            },
+        )
         await ctx.send(response.text)
 
 # Cog 載入 Bot 中
